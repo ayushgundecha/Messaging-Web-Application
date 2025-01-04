e3b1eb97b132db19bf57c052bffc41e5928277f6
ayushgundecha <ayushgundecha123@gmail.com>
2025-01-04 21:20:47 +0530

    updated build

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     cors({
+      origin : true,
       credentials: true, // Allow credentials (cookies) to be sent
     })
   );
